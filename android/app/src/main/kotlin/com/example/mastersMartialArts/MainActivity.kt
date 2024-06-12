c4d1fff1f89ae0a7944d1084867a5a7314c06218
Michael Bastos <mbastos@zcoregroup.com>
2024-06-11 20:49:12 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/mastersMartialArts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mastersMartialArts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+import 'package:mastersMartialArts/config/router.dart';
+import 'package:mastersMartialArts/theme/color_themes.dart';
+import 'package:mastersMartialArts/theme/text_themes.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+void main() => runApp(const MastersMartialArtsApp());
+
+/// The main app.
+class MastersMartialArtsApp extends StatelessWidget {
+  /// Constructs a [MastersMartialArtsApp]
+  const MastersMartialArtsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: "Master's Martial Arts",
+      routerConfig: router,
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: lightColorScheme,
+          textTheme: textTheme),
+      darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: darkColorScheme,
+          textTheme: textTheme),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
+    );
+  }
+}
